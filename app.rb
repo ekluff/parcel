cc29efb51e4e8f9096c41d55e2606c0b654f01fb
Guest User <student@epicodus.com>
2015-08-10 16:50:39 -0700

    Add algorithm in result page to calculate parcel volume and cost to ship.

--- app.rb
+++ app.rb
@@ -8,8 +8,14 @@ get ('/') do
 end
 
 get('/result') do
-
-
+  @length = params.fetch('length')
+  @width = params.fetch('width')
+  @height = params.fetch('height')
+  @distance = params.fetch('distance')
+  @speed = params.fetch('speed')
+  @package = Parcels.new()
+  @volume = @package.volume
+  @cost_to_ship = @package.cost_to_ship(@distance, @speed)
 
   erb(:result)
 end
