71d693486df1679d6b7abeb047d63f1b425918f1
Guest User <student@epicodus.com>
2015-08-10 16:13:25 -0700

    Add second spec. Return the cost to ship the parcel based on volume, weight, distance, and speed of delivery.

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -2,10 +2,17 @@ require('rspec')
 require('parcel')
 
 describe(Parcels) do
-  describe('#parcel') do
+  describe('#volume') do
     it('returns the product of three numbers') do
       test_parcel = Parcels.new(10,10,20,0)
       expect(test_parcel.volume).to(eq(2000))
+    end
+  end
+
+  describe('#cost_to_ship') do
+    it('returns the cost to ship a parcel based on volume and weight') do
+      test_parcel = Parcels.new(10, 10, 20, 100)
+      expect(test_parcel.cost_to_ship(100, 2)).to(eq(32)) #volume/100 + (weight/100) + distance/10 + days/2
     end
   end
 end
