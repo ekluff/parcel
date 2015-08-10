c549707331523df8a02a2f401dfcfb348bd964fc
Guest User <student@epicodus.com>
2015-08-10 16:34:00 -0700

    Write integration spec. It will test that web application processes user input and returns volume of package and cost to ship.

--- spec/parcel_integration_spec.rb
+++ spec/parcel_integration_spec.rb
@@ -4,6 +4,17 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("the triangle path", {:type => :feature}) do
-  # code
+describe('the parcel path', {:type => :feature}) do
+  it('processes the user entry') do
+    visit('/')
+    fill_in('length', :with => '10')
+    fill_in('width', :with => '10')
+    fill_in('height', :with => '20')
+    fill_in('weight', :with => '100')
+    fill_in('days', :with => '3')
+    fill_in('distance', :with => '100')
+    click_button('Go')
+    expect(page).to have_content('2000 cubic centimeters')
+    expect(page).to have_content('$32')
+  end
 end
