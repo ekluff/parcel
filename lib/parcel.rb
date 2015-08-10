4c0c68754abb6893cebe2454caa97d0d6d225a34
Guest User <student@epicodus.com>
2015-08-10 16:24:28 -0700

    Create method cost_to_ship. It will return the value from the formula @volume/100 + @weight/100 + distance/10 + days_to_delivery/2.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,12 +1,17 @@
 class Parcels
   define_method(:initialize) do |length, width, height, weight|
-    @length = length
-    @width = width
-    @height = height
+    # @length = length
+    # @width = width
+    # @height = height
     @weight = weight
+    @volume = length * width * height
   end
 
   define_method(:volume) do
-    volume = @length * @width * @height
+    @volume
+  end
+
+  define_method(:cost_to_ship) do |distance, days_to_delivery|
+    @cost_to_ship = @volume/100 + @weight/100 + distance/10 + days_to_delivery/2
   end
 end
