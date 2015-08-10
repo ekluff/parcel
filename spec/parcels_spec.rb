79aad71d02a4042982542b386138c019be298b58
Guest User <student@epicodus.com>
2015-08-10 15:54:02 -0700

    Write first spec. It will test that method will accept three numbers and return product.

--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,6 +3,9 @@ require('parcel')
 
 describe(Parcels) do
   describe('#parcel') do
-    # code
+    it('returns the product of three numbers') do
+      test_parcel = Parcels.new(10,10,20)
+      expect(test_parcel.parcel).to(eq(2000))
+    end
   end
 end
